e544ba62ebf73cb9d6053f9a796d11a5f3236042
zhabinka <sergeykarpuk@yandex.by>
2018-10-11 01:33:39 +0300

    add yaml file format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,13 +6,13 @@ import genDiff from '../src';
 const result = fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8');
 
 test('JSON diff configuration', () => {
-  const before = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/before.json'));
-  const after = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/after.json'));
+  const before = './__tests__/__fixtures__/before.json';
+  const after = './__tests__/__fixtures__/after.json';
   expect(genDiff(before, after)).toBe(result);
 });
 
 test('YAML diff configuration', () => {
-  const beforeYAML = yaml.safeLoad(fs.readFileSync('./__tests__/__fixtures__/before.yml', 'utf-8'));
-  const afterYAML = yaml.safeLoad(fs.readFileSync('./__tests__/__fixtures__/after.yml', 'utf-8'));
+  const beforeYAML = './__tests__/__fixtures__/before.yml';
+  const afterYAML = './__tests__/__fixtures__/after.yml';
   expect(genDiff(beforeYAML, afterYAML)).toBe(result);
 });
--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 import _ from 'lodash';
 import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 
-const genDiff = (before, after) => {
-  const aa = Object.entries(before).map(([key, value]) => {
-    if (_.has(after, key)) {
-      if (value === after[key]) {
-        return `    ${key}: ${value}`;
+const beforeJSON = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/before.json'));
+const afterJSON = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/after.json'));
+
+const beforeYAML = yaml.safeLoad(fs.readFileSync('./__tests__/__fixtures__/before.yml', 'utf-8'));
+const afterYAML = yaml.safeLoad(fs.readFileSync('./__tests__/__fixtures__/after.yml', 'utf-8'));
+
+const genDiff = (file1, file2) => {
+
+  const parseCheck = (file) => {
+    switch (path.extname(file)) {
+      case '.json':
+        return JSON.parse(fs.readFileSync(file, 'utf-8'));
+      case '.yml':
+        return yaml.safeLoad(fs.readFileSync(file, 'utf-8'));
+      default:
+        return;
+    }
+  }
+  console.log(parseCheck(file2))
+  const diff = (before, after) => {
+    const aa = Object.entries(before).map(([key, value]) => {
+      if (_.has(after, key)) {
+        if (value === after[key]) {
+          return `    ${key}: ${value}`;
+        }
+
+        return `  + ${key}: ${after[key]},  - ${key}: ${value}`.split(',');
       }
 
-      return `  + ${key}: ${after[key]},  - ${key}: ${value}`.split(',');
-    }
+      return `  - ${key}: ${value}`;
+    });
 
-    return `  - ${key}: ${value}`;
-  });
+    const bb = Object.entries(after).map(([key, value]) => (!_.has(before, key) ? `  + ${key}: ${value}` : ''));
 
-  const bb = Object.entries(after).map(([key, value]) => (!_.has(before, key) ? `  + ${key}: ${value}` : ''));
+    const result = _.concat('{', aa, bb.filter(x => x), '}', '');
+    const resultStr = _.flatten(result).join('\n');
 
-  const result = _.concat('{', aa, bb.filter(x => x), '}', '');
-  const resultStr = _.flatten(result).join('\n');
+    return resultStr;
+  };
 
-  // Вывод строки в файл для сравнения с тестовым результатом
-  fs.writeFileSync('./src/resultStr.txt', _.flatten(result).join('\n'), 'utf-8');
-
-  return resultStr;
+  return diff(parseCheck(file1), parseCheck(file2));
 };
 
 export default genDiff;
