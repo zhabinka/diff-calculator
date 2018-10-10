f1aed6bc6b3f8acd127fdfdcfc70a207ff79768a
zhabinka <sergeykarpuk@yandex.by>
2018-10-10 11:53:36 +0300

    linter error correct

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,7 @@
 // @flow
-
-import genDiff from '../src';
 import fs from 'fs';
-/*
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+import genDiff from '../src';
 
-test('null', () => {
-  const n = undefined;
-  expect(n).toBeUndefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
-})
-*/
 test('diff configuration', () => {
   const before = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/before.json'));
   const after = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/after.json'));
--- src/index.js
+++ src/index.js
@@ -4,29 +4,25 @@ import fs from 'fs';
 const genDiff = (before, after) => {
   const aa = Object.entries(before).map(([key, value]) => {
     if (_.has(after, key)) {
-        if (value === after[key]) {
-          return `    ${key}: ${value}`
-        } else {
-          return `  + ${key}: ${after[key]},  - ${key}: ${value}`.split(',')
-        }
-    } else {
-        return `  - ${key}: ${value}`
+      if (value === after[key]) {
+        return `    ${key}: ${value}`;
+      }
+
+      return `  + ${key}: ${after[key]},  - ${key}: ${value}`.split(',');
     }
+
+    return `  - ${key}: ${value}`;
   });
 
-  const bb = Object.entries(after).map(([key, value]) => {
-    if (!_.has(before, key)) {
-      return `  + ${key}: ${value}`;
-    }
-  });
+  const bb = Object.entries(after).map(([key, value]) => (!_.has(before, key) ? `  + ${key}: ${value}` : ''));
 
   const result = _.concat('{', aa, bb.filter(x => x), '}', '');
   const resultStr = _.flatten(result).join('\n');
 
   // Вывод строки в файл для сравнения с тестовым результатом
-  fs.writeFileSync('./src/ii.txt', _.flatten(result).join('\n'), 'utf-8');
+  fs.writeFileSync('./src/resultStr.txt', _.flatten(result).join('\n'), 'utf-8');
 
-  return resultStr; 
-}
+  return resultStr;
+};
 
 export default genDiff;
