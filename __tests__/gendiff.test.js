0ab236d1cac169a825a8ae809dc5dcbc2a160a1e
zhabinka <sergeykarpuk@yandex.by>
2018-10-11 01:13:02 +0300

    fun universal tests :)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,18 @@
 // @flow
 import fs from 'fs';
+import yaml from 'js-yaml';
 import genDiff from '../src';
 
-test('diff configuration', () => {
+const result = fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8');
+
+test('JSON diff configuration', () => {
   const before = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/before.json'));
   const after = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/after.json'));
-  const result = fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8');
   expect(genDiff(before, after)).toBe(result);
+});
+
+test('YAML diff configuration', () => {
+  const beforeYAML = yaml.safeLoad(fs.readFileSync('./__tests__/__fixtures__/before.yml', 'utf-8'));
+  const afterYAML = yaml.safeLoad(fs.readFileSync('./__tests__/__fixtures__/after.yml', 'utf-8'));
+  expect(genDiff(beforeYAML, afterYAML)).toBe(result);
 });
