9f2172fb7f37608b74f63686181b945a47c5db9d
zhabinka <sergeykarpuk@yandex.by>
2018-10-11 19:18:09 +0300

    key/value list format

--- src/parsers.js
+++ src/parsers.js
@@ -3,13 +3,13 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parsersList = new Map([
-  ['.json', JSON.parse],
-  ['.yml', yaml.safeLoad],
-  ['.ini', ini.parse],
-]);
+const parsersList = {
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
+};
 
 export default (file) => {
-  const parser = parsersList.get(path.extname(file));
+  const parser = parsersList[path.extname(file)];
   return parser(fs.readFileSync(file, 'utf-8'));
 };
