9e5ac74ba4c231309450349e8191a942ede12194
zhabinka <sergeykarpuk@yandex.by>
2018-10-14 04:47:34 +0300

    rename function by verb

--- src/format.js
+++ src/format.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
-import parsers from './parsers';
+import parse from './parsers';
 
 const getFormat = file => path.extname(file);
 const getData = file => fs.readFileSync(file, 'utf-8');
 
-export default file => parsers(getData(file), getFormat(file));
+export default file => parse(getData(file), getFormat(file));
