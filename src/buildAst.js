f1a8aab74181078a6b5b7a2ea173de57db79b886
zhabinka <sergeykarpuk@yandex.by>
2018-10-16 23:25:33 +0300

    linter fixed

--- src/buildAst.js
+++ src/buildAst.js
@@ -3,38 +3,42 @@ import _ from 'lodash';
 const nodeList = [
   {
     type: 'nest',
-    check: (key, before, after) => (_.isObject(before[key]) && _.isObject(after[key])),
-    f: ({ key, type, before, after }, func) => ({ key, type, children: func(before[key], after[key]) }),
+    check: (key, before, after) => _.isObject(before[key]) && _.isObject(after[key]),
+    f: (key, type, before, after, func) => ({ key, type, children: func(before[key], after[key]) }),
   },
   {
     type: 'added',
-    check: (key, before, after) => (_.has(after, key) && !_.has(before, key)),
-    f: ({ key, type, before, after }) => ({ key, type, value: after[key] }),
+    check: (key, before, after) => _.has(after, key) && !_.has(before, key),
+    f: (key, type, before, after) => ({ key, type, value: after[key] }),
   },
   {
     type: 'deleted',
-    check: (key, before, after) => (!_.has(after, key) && _.has(before, key)),
-    f: ({ key, type, before }) => ({ key, type, value: before[key] }),
+    check: (key, before, after) => !_.has(after, key) && _.has(before, key),
+    f: (key, type, before) => ({ key, type, value: before[key] }),
   },
   {
     type: 'unchanged',
-    check: (key, before, after) => (_.has(after, key) && _.has(before, key) && before[key] === after[key]),
-    f: ({ key, type, before }) => ({ key, type, value: before[key] }),
+    check: (key, before, after) => _.has(after, key) && _.has(before, key)
+      && before[key] === after[key],
+    f: (key, type, before) => ({ key, type, value: before[key] }),
   },
   {
     type: 'changed',
-    check: (key, before, after) => (_.has(after, key) && _.has(before, key) && before[key] !== after[key]),
-    f: ({ key, type, before, after }) => ({ key, type, valueBefore: before[key], valueAfter: after[key] }),
-  }
+    check: (key, before, after) => _.has(after, key) && _.has(before, key)
+      && before[key] !== after[key],
+    f: (key, type, before, after) => ({
+      key, type, valueBefore: before[key], valueAfter: after[key],
+    }),
+  },
 ];
 
 const buildAst = (before, after) => {
   const keysUnion = _.union(Object.keys(before), Object.keys(after));
 
   const ast = keysUnion.map((key) => {
-    const { type, f } = nodeList.find((el) => el.check(key, before, after));
+    const { type, f } = nodeList.find(el => el.check(key, before, after));
 
-    return f({ key, type, before, after }, buildAst);
+    return f(key, type, before, after, buildAst);
   });
 
   return ast;
